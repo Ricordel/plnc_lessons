313c3a7ae1a9db5d3d7b69b852ef4f31bd124fa3
Yoann Ricordel <leyaude@gmail.com>
2011-05-09 10:03:32 +0200

    Première TH de cours 9 mai, covariance, json, ...

--- scala/src/cours.scala
+++ scala/src/cours.scala
@@ -120,5 +120,122 @@ List(1, 2, 3)
 
 /* Ex de pattern matching, qui utilise impliticement unapply() */
 x.match
-        | case List(y) => println("x = [" + y + "]")
-        | case _ => println("Pas une liste d'un élément")
+         case List(y) => println("x = [" + y + "]")
+         case _ => println("Pas une liste d'un élément")
+
+
+
+
+/** Le pattern matching **/
+val persons = List(Person("John", "Doe"), Person("victor", "Perron"), Person("truc", "bidule"))
+
+persons foreach (_ match {
+    case Person(first, last) if first.startswith("J") => println(last)
+    case x => println("No match for " + x)
+})
+
+
+/* Si on veut afficher l'objet lui-même : on peut recréer un objet Personne
+ * mais c'est moche. Mieux : */
+
+persons foreach (_ match {
+    case p @ Person(first, last) if first.startswith("J") => println(p) // p est l'objet Personne
+    case x => println("No match for " + x)
+})
+
+Some(3) match {
+    case Some(x: Int) => println(x)
+    case _ => ()
+}
+/* Remplacer le début par Some("foobar"), le compilateur sait statiquement que le match
+ * n'a pas de sens, et refuse de compiler */
+ 
+
+
+
+for (x : Int <- Some(3)) println(x)
+/* Fonctionne car for revient à appeler des map, foreach, ..., qui "déshabillent le Some" */
+
+for (Person("Jonh", last) <- persons) println(last) // Le for a fait un pattern matching tout seul
+
+/* Plus loin : ne fait rien cas la Map est vide => retourne un None => ne fait rien
+ * Si la Map n'est pas vide mais ne contient pas les infos pour tout le monde, ça n'affiche
+ * que ceux dont l'âge est entré, car sinon il y a un None dans la chaine => continue; */
+for (p @ Person("Jonh", last) <- persons; age <- (Map[String, Int]()).get(last)) println(last)
+
+
+/** Créer son itérateur **/
+def js = for (p @ Person("Jonh", last) <- persons; age <- (Map[String, Int]()).get(last)) yield(p)
+for (j <- js) println(j)
+
+
+
+/** Nos Some et None, mieux que la dernière fois **/
+
+/* En faisant ça, on profite du fait que getters et setters sont créés automatiquement
+ * dans les case-class pour surcharger la méthode get() du type O */
+case class S[T] (override val get: T) extends O[T] {
+    val isEmpty = false
+}
+
+
+/** Types paramétrés **/
+val s : S[Any] = (S(3) : S[Int]) // ne marche pas et ne doit pas marcher
+
+
+/******************* Covariance et contravariance ***************************/
+
+/* Introduire de la covariance de type : paramétrer par [+T] au lieu de [T] =>
+ * O[B] est maintenant un enfant de O[A] si B est un enfant de A */
+
+
+/* Un cas de mauvaise utilisation de la covariance => le compilo râle, et dit que
+* le type covariant est en position contravariante (= inverse de covariant)
+* Pour se mettre en mode contravariant, on met un - à la place du + */
+abstract class OC[+T] {
+    def send(x : T) : Unit
+}
+
+
+/* Ni coraviant, ni contravariant */
+case class Var[T] (var x : T)
+
+/* Covariant : */
+case class Var[+T] (var x : T)
+/* Ne marche pas : le setter d'un Var[Int] par exemple, ne peut prendre QUE un Int
+ * donc la covariance ne convient pas */
+
+/* Contravariant : */
+case class Var[-T] (var x : T)
+/* Ne marche pas non plus, mais cette fois c'est le getter qui pose problème, lui est dans
+ * une position covariante */
+
+
+/* Que peut-on faire avec ça ? */
+ def f(a : A) : B
+
+ def f(a : A1) : B // A1 dérive de B => ne marche pas, position contravariante : on n'est plus capable de prendre n'importe quel A
+ def f(a : Any) : B // OK
+ def f(a : A) : Any // Non => problème inverse
+ def f(a : A) : B1 // OK => on retourne un B1, donc effectivement un B, on n'a jamais dit qu'on devait être surjectif sur B
+
+
+
+/***** Écrivons notre classe pour remplacer les listes *****/
+abstract class L[+T] {
+    def ::[U >: T] (elem : U) : L[U] = Link(elem, this) // Attention, les opérateurs finissant en : appellent la méthode de l'élément de droite
+}
+
+/* La notation >: T veut dire "ancêtre" (au sens large). => U sera alors un type commun au type qu'on veut
+ * insérer, et à T. On prend le plus proche ancêtre commun 
+ * Il existe pareil pour U <: T, qui veut dire U dérive de T. C'est souvent implicite, mais peut être utile */
+
+case class Link[+T] (val head : T, val tail : L[T]) extends L[T]
+
+case object E extends L[Nothing]
+
+
+
+
+
+
---
+++ scala/src/json.scala
@@ -0,0 +1,36 @@
+/*************************** Faire du JSON avec scala ***************************/
+/* Ajouter du lift_json dans le paramétrage du projet */
+
+import net.liftweb.json._
+import net.liftweb.jsonParser._
+
+
+def f(implicit toto : Int) = println(toto)
+implicit val x = 3 // Si c'est le seul implicit Int dans le scope => paramètre automatique
+                   // si on appelle f sans argument               
+
+/* Le parseul JSON est codé pareil : il y a un defaultFormat */
+implicit val formats = DefaultFormats
+
+
+// Si el type spécifié à extract convient bien à l'objet passé, 
+// tout est automatique. S'il y a des champs en trop, ils sont ignorés
+parse("""{"first" : "Jonh", "last" : "Doe"}""").extract[Person]
+
+// bien sûr, il y a pareil pour sérialiser
+import net.liftweb.json.Serialisation.write
+write(Person("Sam", "Tardieu")) // va écrire l'objet JSON correspondant
+
+
+// Faire la requête à l'API : utiliser les lib Java qui vont bien
+// ou bien scala.io.Source.fromURL. Il existe aussi fromFile()
+// Puis on peut utiliser split(), getLines(), ...
+
+import scala.xml._
+/* Maintenant il y a une tonne de trucs implicites super pratiques */
+val x = <person><first>Jonh</first><last>Doe</last></person>
+x \ "first" // retourne une séquence de noeuds
+
+(x \ "first") text // Retourne John
+
+(x \ "_") text // Retourne JonhDoe
